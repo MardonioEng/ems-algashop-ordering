7ceac193830d17769954d3046f55e06851f465e1
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2026-02-16 20:05:26 -0300

    implementing Order and OrderItem

---
+++ src/main/java/br/com/mardoniorodrigues/ordering/domain/entity/OrderStatus.java
@@ -0,0 +1,10 @@
+package br.com.mardoniorodrigues.ordering.domain.entity;
+
+public enum OrderStatus {
+
+    DRAFT,
+    PLACED,
+    PAID,
+    READY,
+    CANCELED
+}
---
+++ src/main/java/br/com/mardoniorodrigues/ordering/domain/entity/PaymentMethod.java
@@ -0,0 +1,7 @@
+package br.com.mardoniorodrigues.ordering.domain.entity;
+
+public enum PaymentMethod {
+
+    CREDIT_CARD,
+    GATEWAY_BALANCE
+}
