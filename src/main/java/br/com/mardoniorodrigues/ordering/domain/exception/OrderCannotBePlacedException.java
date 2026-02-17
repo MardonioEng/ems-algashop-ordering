1f0461946c10a845b0c116f6e9688aea5f491e7f
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2026-02-17 09:11:35 -0300

    enhancing Exceptions with Factory Method

--- src/main/java/br/com/mardoniorodrigues/ordering/domain/exception/OrderCannotBePlacedException.java
+++ src/main/java/br/com/mardoniorodrigues/ordering/domain/exception/OrderCannotBePlacedException.java
@@ -2,11 +2,52 @@ package br.com.mardoniorodrigues.ordering.domain.exception;
 
 import br.com.mardoniorodrigues.ordering.domain.valueObject.id.OrderId;
 
-import static br.com.mardoniorodrigues.ordering.domain.exception.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS;
+import static br.com.mardoniorodrigues.ordering.domain.exception.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_ITEMS;
+import static br.com.mardoniorodrigues.ordering.domain.exception.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_SHIPPING_INFO;
+import static br.com.mardoniorodrigues.ordering.domain.exception.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_BILLING_INFO;
+import static br.com.mardoniorodrigues.ordering.domain.exception.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_INVALID_SHIPPING_COST;
+import static br.com.mardoniorodrigues.ordering.domain.exception.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_INVALID_DELIVERY_DATE;
+import static br.com.mardoniorodrigues.ordering.domain.exception.ErrorMessages.ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_PAYMENT_METHOD;
 
 public class OrderCannotBePlacedException extends DomainException{
 
-    public OrderCannotBePlacedException(OrderId id) {
-        super(String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS, id));
+    private OrderCannotBePlacedException(String message) {
+        super(message);
+    }
+
+    public static OrderCannotBePlacedException noItems(OrderId id) {
+        return new OrderCannotBePlacedException(
+            String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_ITEMS, id)
+        );
+    }
+
+    public static OrderCannotBePlacedException noShippingInfo(OrderId id) {
+        return new OrderCannotBePlacedException(
+                String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_SHIPPING_INFO, id)
+        );
+    }
+
+    public static OrderCannotBePlacedException noBillingInfo(OrderId id) {
+        return new OrderCannotBePlacedException(
+                String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_BILLING_INFO, id)
+        );
+    }
+
+    public static OrderCannotBePlacedException invalidShippingCost(OrderId id) {
+        return new OrderCannotBePlacedException(
+                String.format(ERROR_ORDER_CANNOT_BE_PLACED_INVALID_SHIPPING_COST, id)
+        );
+    }
+
+    public static OrderCannotBePlacedException invalidExpectedDeliveryDate(OrderId id) {
+        return new OrderCannotBePlacedException(
+                String.format(ERROR_ORDER_CANNOT_BE_PLACED_INVALID_DELIVERY_DATE, id)
+        );
+    }
+
+    public static OrderCannotBePlacedException noPaymentMethod(OrderId id) {
+        return new OrderCannotBePlacedException(
+                String.format(ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_PAYMENT_METHOD, id)
+        );
     }
 }
