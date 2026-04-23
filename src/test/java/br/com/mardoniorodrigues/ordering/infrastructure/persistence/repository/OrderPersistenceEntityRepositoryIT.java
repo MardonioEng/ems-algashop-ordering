5be000d34bc369c794f46dc04a8b0622af2e59a9
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2026-04-23 08:01:16 -0300

    isolating the execution of integration tests

---
+++ src/test/java/br/com/mardoniorodrigues/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -0,0 +1,12 @@
+package br.com.mardoniorodrigues.ordering.infrastructure.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPersistenceEntityRepositoryIT {
+
+    @Test
+    public void shouldRun() {
+    }
+}
